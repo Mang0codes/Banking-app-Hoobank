5f78c47989f3d7415066d42a6568cf838fd841b0
My Name <albiahossain0@gmail.com>
2024-08-24 16:43:17 +0530

    dist source added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  bas: "/Banking-app-Hoobank/",
+  base: "/Banking-app-Hoobank/",
   plugins: [react()],
+  build: {
+    outDir: 'dist', 
+  },
 })
